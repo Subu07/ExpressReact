65e2f8984e24baaaac85ffd8725becce56f66ccb
regaron <rabigiri18@gmail.com>
2018-06-30 09:09:00 +0545

    post added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,15 +3,40 @@ import './App.css';
 import Navbar from "./Header/Navbar/Navbar";
 
 class App extends Component {
-  /*  state = {
-        info: []
-    };
-
-*/
+    constructor(props) {
+        super(props);
+        this.state = {
+            formData: []
+        };
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
     componentDidMount() {
           //  this.fetchData();
-
     }
+    sendData = (data) => {
+        fetch("/send",{
+            body: JSON.stringify(data),
+            headers: {'content-type' : 'application/json',},
+            method: 'POST'
+        })
+            .then(res => console.log(res.json()))
+    };
+
+    handleChange = (event) => {
+        this.setState({
+            formData: event.target.value
+        });
+        event.preventDefault();
+    };
+    handleSubmit = (event) => {
+        console.log(this.state.formData);
+        this.sendData(this.state.formData);
+        this.setState({
+            formData: []
+        });
+        event.preventDefault();
+    };
    /* fetchData = () => {
         fetch("/check")
             .then(response => response.json())
@@ -43,6 +68,10 @@ class App extends Component {
     return (
       <div className="App">
         <Navbar/>
+          <form onSubmit={this.handleSubmit}>
+              <input type={"text"} onChange={this.handleChange}/>
+              <button type={"submit"}>Submit</button>
+          </form>
       </div>
     );
   }
